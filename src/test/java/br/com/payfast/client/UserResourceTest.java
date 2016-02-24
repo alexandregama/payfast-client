db0afe5a1eb3996ce9928a69e4b9a395925f15d8
alexandregama <alexandre.gama.lima@gmail.com>
2016-02-23 22:26:50 -0300

    Adds a new Endpoint to retrieve a paymento from its id using ClientBuilder

---
+++ src/test/java/br/com/payfast/client/UserResourceTest.java
@@ -0,0 +1,21 @@
+package br.com.payfast.client;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.client.ClientBuilder;
+
+import org.junit.Test;
+
+public class UserResourceTest {
+
+	@Test
+	public void shouldRetrieveAnUserByItsId() throws Exception {
+		Payment payment = ClientBuilder.newClient()
+			.target("http://127.0.0.1:8080/payfast/payments/1")
+			.request()
+			.accept("application/json")
+			.get(Payment.class);
+		
+		assertEquals(1L, payment.getId(), 0);
+	}
+}
